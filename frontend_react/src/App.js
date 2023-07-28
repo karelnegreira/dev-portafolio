5c432ce656cc4832584e28d4ec7c118af1f0124e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-07-29 10:28:02 +1200

    first commit navbar partially implemented

---
+++ frontend_react/src/App.js
@@ -0,0 +1,22 @@
+
+
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <About />
+      <Footer />
+      <Header />
+      <Skills />
+      <Testimonial />
+      <Work />
+    </div>
+  );
+}
+
+export default App;
