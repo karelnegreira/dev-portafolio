a485abbc16101f4e3dd1eb4f36fc9a322404fc19
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-08-14 17:52:52 +1200

    portafolio api is set and ready

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -6,7 +6,7 @@ export const client = sanityClient({
     dataset: 'production', 
     apiVersion: '2023-07-30', 
     useCdn: true, 
-    token: process.env.SANITY_PROJECT_TOKEN 
+    token: 'skfLMUVx1MaWIXs6S9sc7rB36TSZwSDj6Yv6lYMaPA0V3z7RlZHm8ZN0zYv12qwPl7nCKRFx2P3zoCqBO7HVyC4XSkiBoLNkZezTEiWANnYKCUnIt3AeZw3P3w2AEPMXlWHB59IP9OpNhmtX0SIUyaR7dz52HUEgLCEaAxO0OeKoElWPJaE3', 
 });
 
 const builder = imageUrlBuilder(client);
--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,10 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { images } from '../../constants';
+import { AppWrap, MotionWrap } from '../../wrapper';
+import { client } from '../../client';
+
 import './Footer.scss';
 
 const Footer = () => {
+
+  const [formData, setFormData] = useState({name: '', email: '', message: ''});
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const {name, email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const {name, value } = e.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+    const contact = {
+      _type: 'contact', 
+      name: formData.name, 
+      email: formData.email, 
+      message: formData.message, 
+    };
+
+
+
+    client.create(contact)
+          .then(() => {
+            setLoading(false);
+            setIsFormSubmitted(true);
+          })
+          .catch((err) => console.log(err));
+  };
+
   return (
-    <div>Footer</div>
+    <>
+      <h4 className="head-text">Take five minutes and chat with me! </h4>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email"/>
+          <a href="mailto: karelnegreira@gmail.com" className="p-text">karelnegreira@gmail.com</a>
+        </div>
+
+        <div className="app__footer-card">
+          <img src={images.mobile} alt="mobile"/>
+          <a href="tel: +679 8950396" className="p-text">+679 - 895 - 0396</a>
+        </div>
+      </div>
+      {!isFormSubmitted ? (
+      <div className="app__footer-form app__flex">
+        <div className="app-flex">
+          <input className="p-text" 
+              type="text" 
+              placeholder="your name" 
+              name="name"
+              value={name} 
+              onChange={handleChangeInput} 
+          />
+        </div>
+        <div className="app-flex">
+          <input className="p-text" 
+              type="text" 
+              placeholder="your email" 
+              name="email"
+              value={email} 
+              onChange={handleChangeInput} 
+          />
+        </div>
+        <div>
+          <textArea 
+            className="p-text"
+            placeholder="Your message"
+            value={message}
+            name="message"
+            onChange={handleChangeInput}
+          />
+        </div>
+        <button className="p-text" onClick={handleSubmit}> { !loading ? 'Send message' : 'Sending...' } </button>
+      </div>
+      ) : ( 
+      <div> 
+        <h3 className="head-text">Thank you for getting in touch!</h3>
+      </div>
+       )}
+    </>
   )
 }
 
-export default Footer
+export default AppWrap(MotionWrap(Footer, 'app__footer'), 'contact', 'app_whitebg');
